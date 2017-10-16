9272979d910436177ea4b5cc0abfe3d07c76a4bd
illiaberdnyk <32646855+illiaberdnyk@users.noreply.github.com>
2017-10-17 00:24:08 +0300

    Ready

--- src/Summ.java
+++ src/Summ.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
-
 public class Summ {
     public static void main (String [] args) {
         int x=0;
        for(int i=1; i<=10; i++){
-            x = x+i;
+            x += i;
         }
 
        System.out.println(x);
