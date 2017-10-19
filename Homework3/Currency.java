dc280e3f4edd528d0db803a228a97c49d997da91
Kinvic <32065060+Kinvic@users.noreply.github.com>
2017-10-19 12:09:18 +0300

    Update Currency.java

--- Homework3/Currency.java
+++ Homework3/Currency.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.util.regex.*;
+/*Implement a pattern for US currency: the first symbol "$", then any number of digits, dot and two digits after the dot. Enter the text from the console that contains several occurrences of US currency. Display all occurrences on the screen.*/
+
 public class Currency {
     public static void main(String... args){
         Scanner sc=new Scanner(System.in);
