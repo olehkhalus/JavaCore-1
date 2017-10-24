8d83443f874bfc902ddcf2e18e88d058e74e1982
D Pl <plantusv@ukr.net>
2017-10-24 11:19:25 +0300

    remove bark() and meow()

--- src/main/com/hw/hw5/animal/Cat.java
+++ src/main/com/hw/hw5/animal/Cat.java
@@ -3,13 +3,9 @@ package com.hw.hw5.animal;
 public class Cat implements Animal {
     
     public String voice() {
-        return meow();
-    }
-
-    public String meow() {
         return "Miaow";
     }
-    
+
     public String feed() {
         return "Yummy!";
     }
--- src/main/com/hw/hw5/animal/Dog.java
+++ src/main/com/hw/hw5/animal/Dog.java
@@ -3,11 +3,7 @@ package com.hw.hw5.animal;
 public class Dog implements Animal {
     
     public String voice() {
-        return bark();
-    }
-
-    private String bark() {
-        return "Woof woof woof";
+        return "Woof";
     }
     
     public String feed() {
