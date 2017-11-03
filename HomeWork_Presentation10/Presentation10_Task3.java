3fd0903fec95cdcb5cebe159d69632cad781b95d
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-03 17:40:06 +0200

    Update Presentation10_Task3.java

--- HomeWork_Presentation10/Presentation10_Task3.java
+++ HomeWork_Presentation10/Presentation10_Task3.java
@@ -4,23 +4,24 @@ package HomeWork_Presentation10;
 // which has to output its number («Thread number two») 3 times and create thread «three»,
 // which would to output message «Thread number three» 5 times.
 
-class T extends Thread {
-    String number ;
+class MyThread extends Thread {
+    String name ;
     int iterations;
 
-    T( String number , int iterations) {
-        this.number = number;
+    MyThread( String name , int iterations) {
+        this.name = name;
         this.iterations = iterations;
     }
 
     public void run(){
+      	
         for ( int i = 0 ; i < iterations ; i++) {
-            System.out.println("Thread number" + number);
+            System.out.println("Thread number " +name+" "+this.getName());
         }
         if ( iterations == 0 ) {
-            new T(" two" , 3).run();
+            new MyThread(" two" , 3).start();
         } else if ( iterations == 3 ) {
-            new T(" three" , 5 ).run();
+            new MyThread(" three" , 5 ).start();
         } else if ( iterations > 3 ) {
             System.out.println("Finaly!!!!");
         }
@@ -32,13 +33,9 @@ class T extends Thread {
 
 public class Presentation10_Task3 {
     public static void main( String[] Args) {
-        T thread1 = new T ("one" , 0);
+        MyThread thread1 = new MyThread ("one" , 0);
         
-
-        thread1.run();
+        thread1.start();
         
-
-
-
     }
 }
