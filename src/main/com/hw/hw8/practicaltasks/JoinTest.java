d815f25fad6f431f9c2e898e2cc372de37c971a9
D Pl <plantusv@ukr.net>
2017-11-03 11:10:49 +0200

    clean up

--- src/main/com/hw/hw8/practicaltasks/JoinTest.java
+++ src/main/com/hw/hw8/practicaltasks/JoinTest.java
@@ -5,25 +5,28 @@ import java.util.stream.*;
 public class JoinTest {
     
     public static void main(String[] args) throws Exception {
-        Runnable stdout1 = () -> {
-            IntStream.range(0, 5).forEach($ -> System.out.printf("Stdout-0 %s%n", Thread.currentThread().getName()));
+        Runnable stdout0 = () -> {
+            IntStream.range(0, 5).forEach($ -> System.out.printf("stdout-0 %s%n", Thread.currentThread().getName()));
         };
-        Thread t1= new Thread(stdout1);
+
+        Thread t0 = new Thread(stdout0);
+
+        Runnable stdout1 = () -> {
+            IntStream.range(0, 5).forEach($ -> System.out.printf("stdout-1 %s%n", Thread.currentThread().getName()));
+        };
+
+        Thread t1 = new Thread(stdout1);
 
         Runnable stdout2 = () -> {
-            IntStream.range(0, 5).forEach($ -> System.out.printf("Stdout-1 %s%n", Thread.currentThread().getName()));
+            IntStream.range(0, 5).forEach($ -> System.out.printf("stdout-2 %s%n", Thread.currentThread().getName()));
         };
+
         Thread t2 = new Thread(stdout2);
 
-        Runnable stdout3 = () -> {
-            IntStream.range(0, 5).forEach($ -> System.out.printf("Stdout-2 %s%n", Thread.currentThread().getName()));
-        };
-        Thread t3 = new Thread(stdout3);
-
+        t0.start();
         t1.start();
-        t2.start();
         t2.join();
-        t3.start();
+        t2.start();
     }
 }
 
--- src/main/com/hw/hw8/practicaltasks/ThreadSleepLab.java
+++ src/main/com/hw/hw8/practicaltasks/ThreadSleepLab.java
@@ -4,7 +4,8 @@ public class ThreadSleepLab {
     
     public static void main(String[] args) {
         stdout("I study Java", 10, 1000);
-        stdout("Hello, world!", 2000, "Peace is the peace", 3000, 5);
+        stdout("Hello, world!", 5, 2000);
+        stdout("Peace is the peace", 5, 3000);
     }
 
     private static void stdout(String s, int count, long pause) {
@@ -17,21 +18,5 @@ public class ThreadSleepLab {
         } catch (InterruptedException ie) {
             Thread.currentThread().interrupt();
         }
-    }
-
-    private static void stdout(String s1, long pause1, String s2, long pause2, int count) {
-        try {
-            for (int i = 0; i < count; i++) {
-                System.out.println(s1);
-                System.out.flush();
-                Thread.sleep(pause1);
-                System.out.println(s2);
-                System.out.flush();
-                Thread.sleep(pause2);
-            }
-        } catch (InterruptedException ie) {
-                Thread.currentThread().interrupt();
-        }
-        System.out.println("My name is ...");
     }
 }
