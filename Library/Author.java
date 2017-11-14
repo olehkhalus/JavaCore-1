916607d9f825529ddcb0c40f462100d2b454a5dd
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-14 16:33:24 +0200

    Update Author.java

--- Library/Author.java
+++ Library/Author.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author {
-	private String AuthorName;
-	private List<Book> AuthorBooks;
-	public Author() {
-		
-	}
-	
-	public Author(String AuthorName , ArrayList<Book> AuthorBooks) {
-		this.AuthorName = AuthorName;
-	}
-	
-	public String getAuthorName() {
-		return AuthorName;
-	}
-	public void setAuthorName(String AuthorName) {
-		this.AuthorName = AuthorName;
-	}
-	
-	public List<Book> getAuthorBooks() {
-		return AuthorBooks;
-	}
-	public void setAuthorBooks(String AuthorName) {
-		this.AuthorBooks = AuthorBooks;
-	}
+    private String fullName; // FN, LN
+    private List<Book> books;
+
+    public Author() {
+
+    }
+
+    public Author(String AuthorName , ArrayList<Book> AuthorBooks) {
+        this.fullName = AuthorName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String AuthorName) {
+        this.fullName = AuthorName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+    public void setBooks(String AuthorName) {
+        this.books = books;
+    }
 }
+
