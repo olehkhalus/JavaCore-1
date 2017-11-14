ec78428306448c2f1685f44d72a56f1bc2e6cab7
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-14 16:33:53 +0200

    Update Book.java

--- Library/Book.java
+++ Library/Book.java
@@ -1,42 +1,49 @@
 package Library;
 
+
+
 public class Book {
-	private String BookName;
-	private String BookAuthor;
-	private int YearOfPublication;
-	
-	public Book() {
-		
-	}
-	
-	public Book(String BookName , String BookAuthor , int YearOfPublication) {
-		this.BookName = BookName;
-		this.BookAuthor = BookAuthor;
-		this.YearOfPublication = YearOfPublication;
-	}
-	
-	public String getBookName() {
-		return BookName;
-	}
-	public void setBookName(String BookName) {
-		this.BookName = BookName;
-	}
-	
-	public String getBookAuthor() {
-		return BookAuthor;
-	}
-	public void setBookAuthor(String BookAuthor) {
-		this.BookAuthor = BookAuthor;
-	}
-	
-	public int getYearOfPublication() {
-		return YearOfPublication;
-	}
-	public void setYearOfPublication(int YearOfPublication) {
-		this.YearOfPublication = YearOfPublication;
-	}
-	
-	public void outputBook() {
-		System.out.println("Book name "+BookName+" Author "+BookAuthor+" year "+YearOfPublication);
-	}
+    // TODO field  MUST started from lowerCase !!!!!!!
+    private String BookName;
+    //TODO Create Author type
+    private String BookAuthor;
+
+    private int YearOfPublication;
+
+    public Book() {
+
+    }
+
+    public Book(String BookName , String BookAuthor , int YearOfPublication) {
+        this.BookName = BookName;
+        this.BookAuthor = BookAuthor;
+        this.YearOfPublication = YearOfPublication;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+    public void setBookName(String BookName) {
+        this.BookName = BookName;
+    }
+
+    public String getBookAuthor() {
+        return BookAuthor;
+    }
+    public void setBookAuthor(String BookAuthor) {
+        this.BookAuthor = BookAuthor;
+    }
+
+    public int getYearOfPublication() {
+        return YearOfPublication;
+    }
+    public void setYearOfPublication(int YearOfPublication) {
+        this.YearOfPublication = YearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Book name "+BookName+" Author "+BookAuthor+" year "+YearOfPublication;
+    }
 }
+
