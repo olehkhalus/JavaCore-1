01f2da5cf53ae6a1852debd854ca1ca3de4eca8c
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-15 19:49:05 +0200

    Update Book.java

--- Library/Book.java
+++ Library/Book.java
@@ -1,45 +1,42 @@
 package Library;
 
-
-
 public class Book {
+    private String name;
+    private Author author;
+    private int yearOfPublication;
 
-	private String name;
-	private Author author;
-	private int yearOfPublication;
-	
-	public Book() {
-		
-	}
-	
-	public Book(String name , Author author , int yearOfPublication) {
-		this.name = name;
-		this.author = author;
-		this.yearOfPublication = yearOfPublication;
-	}
-	
-	public String getBookName() {
-		return name;
-	}
-	public void setBookName(String name) {
-		this.name = name;
-	}
-	
-	public Author getBookAuthor() {
-		return author;
-	}
-	public void setBookAuthor(Author author) {
-		this.author = author;
-	}
-	
-	public int getYearOfPublication() {
-		return yearOfPublication;
-	}
-	public void setYearOfPublication(int yearOfPublication) {
-		this.yearOfPublication = yearOfPublication;
-	}
-	
-	@Override
+    public Book() {
+
+    }
+
+    public Book(String name , Author author , int yearOfPublication) {
+        this.name = name;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getBookName() {
+        return name;
+    }
+    public void setBookName(String name) {
+        this.name = name;
+    }
+
+    public Author getBookAuthor() {
+        return author;
+    }
+    public void setBookAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
     public String toString() {
         return "Book name "+name+" Author "+author.getName()+" year "+yearOfPublication;
     }
