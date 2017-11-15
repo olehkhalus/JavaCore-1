1b64550f9d10d7b5b8b03ae0fb470b9ac5e12de3
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-15 19:46:57 +0200

    Update Test.java

--- Library/Test.java
+++ Library/Test.java
@@ -10,29 +10,29 @@ public class Test {
 
 
         Library one = new Library("Central 147",12,0,23,0);
-        Book book1 = new Book("First","Author1",2000);
+        Book book1 = new Book("First", new Author( "First","Author"),2000);
         one.addBook(book1, 7);
-        one.addBook(new Book("Second","Author2",2005), 3);
+        one.addBook(new Book("Second",new Author( "Second","Author"),2005), 3);
 
-        Library two = new Library("Central 147",12,0, 0,0);
+        Library two = new Library("Central 12",7,0, 0,0);
         two.addBook(book1, 7);
-        two.addBook(new Book("Third","Author2",2005), 1);
+        two.addBook(new Book("Third",new Author( "First","Author"),2005), 1);
 
         Iterator<Map.Entry<Book, Integer>> iterator = one.getBooks().entrySet().iterator();
 
-        while ( iterator.hasNext()) {
+        /*while ( iterator.hasNext()) {
             Map.Entry<Book, Integer> pair = iterator.next();
             Book book = pair.getKey();
             Integer NumberOfBooks = pair.getValue();
             System.out.println(book);
-        }
+        }*/
 
-        LibrariesService LibrariesService = new LibrariesService();
-        LibrariesService.addLibrary(one);
-        LibrariesService.addLibrary(two);
+        LibrariesService librariesService = new LibrariesService();
+        librariesService.addLibrary(one);
+        librariesService.addLibrary(two);
 
-        LibrariesService.findBook(book1);
-
+        librariesService.findBook(book1);
+        System.out.println(librariesService.findBook(book1));
 
     }
 }
