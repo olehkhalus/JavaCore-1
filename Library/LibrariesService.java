03ff60e437243998a72fcdeae13396e7fea9cd58
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-15 19:49:37 +0200

    Update LibrariesService.java

--- Library/LibrariesService.java
+++ Library/LibrariesService.java
@@ -7,59 +7,52 @@ import java.util.Comparator;
 import java.util.List;
 
 public class LibrariesService {
-	
-	private List<Library> listOfLibraries = new ArrayList<>();
-	
-	public LibrariesService() {
-		
-	}
-	
-	public void addLibrary( Library Library ) {
-		listOfLibraries.add(Library);
-	}
-	
-	public void findBook(Book book) {
-		List<Library> res = new ArrayList<Library>();
-		
-		for ( Library library : listOfLibraries) {
-			if (library.isBookInLibrary(book) && library.isBookAvailable(book) ) {
+    private List<Library> listOfLibraries = new ArrayList<>();
+
+    public LibrariesService() {
+
+    }
+
+    public void addLibrary( Library Library ) {
+        listOfLibraries.add(Library);
+    }
+
+    public List<Library> findBook(Book book) {
+        List<Library> res = new ArrayList<Library>();
+
+        for ( Library library : listOfLibraries) {
+            if (library.isBookInLibrary(book) && library.isBookAvailable(book) ) {
                 res.add(library);
-            		}
-		}
-		
-		Collections.sort(res, new Comparator<Library>() {
-			  public int compare(Library o1, Library o2) {
-			    return o1.getOpeningTime().compareTo(o2.getOpeningTime());
-			  }
-			});
-		
-		Collections.reverse(res);
-		
-		for ( Library library : res ) {
-			System.out.println("Library at the adress "+library.getAdress()+". This library opens in :"+" and will close at: "+library.getEndOfWorkTime());
-		}
-	}
-	
-	public void findBookRightNow(Book book) {
-		
-		List<Library> res = new ArrayList<Library>();
-		
-		for (Library library : listOfLibraries) {
+            }
+        }
+
+        Collections.sort(res, new Comparator<Library>() {
+            public int compare(Library o2, Library o1) {
+                return o2.getOpeningTime().compareTo(o1.getOpeningTime());
+            }
+        });
+
+        return res;
+    }
+
+    public List<Library> findBookRightNow(Book book) {
+
+        List<Library> res = new ArrayList<Library>();
+
+        for (Library library : listOfLibraries) {
             if (LocalTime.now().getHour() >= library.getOpeningTime().getHour() && LocalTime.now().getMinute() >= library.getOpeningTime().getMinute() && LocalTime.now().getHour() < library.getEndOfWorkTime().getHour() ) {
                 if (library.isBookInLibrary(book) && library.isBookAvailable(book) ) {
                     res.add(library);
                 }
             }
         }
-		
-		Collections.sort(res, new Comparator<Library>() {
-			  public int compare(Library o1, Library o2) {
-			    return o1.getEndOfWorkTime().compareTo(o2.getEndOfWorkTime());
-			  }
-			});
-		
-		for ( Library library : res ) {
-			System.out.println("Library at the adress "+library.getAdress()+" will close at: "+library.getEndOfWorkTime());
-		}	
-	}
+
+        Collections.sort(res, new Comparator<Library>() {
+            public int compare(Library o1, Library o2) {
+                return o1.getEndOfWorkTime().compareTo(o2.getEndOfWorkTime());
+            }
+        });
+
+        return res;
+    }
 }
