e9bbc16b608723ee477479a55daec6511849e356
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-17 16:04:44 +0200

    Update Test.java

--- Library/Test.java
+++ Library/Test.java
@@ -1,7 +1,13 @@
 package Library;
 
-import java.util.Iterator;
+
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+//import java.util.Iterator;
+//import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
@@ -10,29 +16,34 @@ public class Test {
 
 
         Library one = new Library("Central 147",12,0,23,0);
+        
         Book book1 = new Book("First", new Author( "First","Author"),2000);
+        Book book4 = new Book("Fourth", new Author( "First","Author"),2001);
         one.addBook(book1, 7);
+        one.addBook(book4, 3);
         one.addBook(new Book("Second",new Author( "Second","Author"),2005), 3);
 
         Library two = new Library("Central 12",7,0, 0,0);
         two.addBook(book1, 7);
-        two.addBook(new Book("Third",new Author( "First","Author"),2005), 1);
-
-        Iterator<Map.Entry<Book, Integer>> iterator = one.getBooks().entrySet().iterator();
-
-        /*while ( iterator.hasNext()) {
-            Map.Entry<Book, Integer> pair = iterator.next();
-            Book book = pair.getKey();
-            Integer NumberOfBooks = pair.getValue();
-            System.out.println(book);
-        }*/
+        two.addBook(new Book("Third",new Author( "Third","Author"),2005), 1);
 
         LibrariesService librariesService = new LibrariesService();
         librariesService.addLibrary(one);
         librariesService.addLibrary(two);
-
-        librariesService.findBook(book1);
-        System.out.println(librariesService.findBook(book1));
-
+        
+        Map<Library, String> map = librariesService.findLibraryWhithAuthor("First");
+        
+        for (Map.Entry<Library, String> e  :  map.entrySet()) {
+        	System.out.println(e.getKey()+" "+e.getValue());
+        }
+        
+        System.out.println();
+        
+        List<Library> lib = librariesService.findBook(book1);
+        
+        for ( Library e  :  lib) {
+        	System.out.println(e.toString());
+        }
     }
 }
+
