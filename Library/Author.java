9273c18e351c908c3344b30fcf898fd56a75f17f
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-17 16:01:50 +0200

    Update Author.java

--- Library/Author.java
+++ Library/Author.java
@@ -1,36 +1,71 @@
 package Library;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Author {
-    private String firstName;
-    private String lastName;
-    private List<Book> books;
-    public Author() {
+public class Library {
+    private String adress;
+    private LocalTime openingTime;
+    private LocalTime endOfWorkTime;
+    private Map<Book, Integer > books = new HashMap<>(); // integer is the count of available exemplar of this book in library
+
+    public Library() {
 
     }
-
-    public Author(String firstName , String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Library ( String adress , int openedHour , int openedMinute  , int closedHour , int closedMinute  ) {
+        this.adress = adress;
+        this.openingTime = LocalTime.of(openedHour, openedMinute);
+        this.endOfWorkTime = LocalTime.of(closedHour, closedMinute);
     }
 
-    public String getName() {
-        return firstName+" "+lastName;
+    public String getAdress() {
+        return adress;
     }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setAdress( String adress) {
+        this.adress = adress;
     }
 
-    public List<Book> getBooks() {
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+    public void setLibraryOpeningTime( LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public LocalTime getEndOfWorkTime() {
+        return endOfWorkTime;
+    }
+    public void setEndOfWorkTime( LocalTime endOfWorkTime) {
+        this.endOfWorkTime = endOfWorkTime;
+    }
+
+    public Map<Book, Integer> getBooks() {
         return books;
     }
-    public void addBook(Book book) {
-        books.add(book);
+
+    public void addBook( Book book , Integer NumberOfBooks ) {
+        books.put(book, NumberOfBooks);
+    }
+
+    public boolean isBookInLibrary(Book book) {
+        if (books.containsKey(book)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isBookAvailable(Book book) {
+        if ( books.get(book) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Library adress "+adress+". This library opened in: "+getOpeningTime()+" and closed at:  "+getEndOfWorkTime()+"\n";
     }
 }
