c2daa51771520155b21d3f4311a4e072902911c1
TarasM1990 <32651217+TarasM1990@users.noreply.github.com>
2017-11-17 19:55:18 +0200

    Update Test.java

--- Library/Test.java
+++ Library/Test.java
@@ -16,9 +16,9 @@ public class Test {
 
 
         Library one = new Library("Central 147",12,0,23,0);
-        
-        Book book1 = new Book("First", new Author( "First","Author"),2000);
-        Book book4 = new Book("Fourth", new Author( "First","Author"),2001);
+        Author author1 = new Author("First","Author");
+        Book book1 = new Book("First", author1,2000);
+        Book book4 = new Book("Fourth", author1,2001);
         one.addBook(book1, 7);
         one.addBook(book4, 3);
         one.addBook(new Book("Second",new Author( "Second","Author"),2005), 3);
@@ -30,20 +30,37 @@ public class Test {
         LibrariesService librariesService = new LibrariesService();
         librariesService.addLibrary(one);
         librariesService.addLibrary(two);
-        
-        Map<Library, String> map = librariesService.findLibraryWhithAuthor("First");
-        
+
+        Map<Library, String> map = librariesService.findLibraryWhithAuthorName("First Author");
+
         for (Map.Entry<Library, String> e  :  map.entrySet()) {
-        	System.out.println(e.getKey()+" "+e.getValue());
+            System.out.println(e.getKey()+" "+e.getValue());
         }
-        
+
         System.out.println();
-        
-        List<Library> lib = librariesService.findBook(book1);
-        
-        for ( Library e  :  lib) {
-        	System.out.println(e.toString());
+
+        Map<Library, String> map1 = librariesService.findLibraryWhithAuthorName("Second Author");
+
+        for (Map.Entry<Library, String> e  :  map1.entrySet()) {
+            System.out.println(e.getKey()+" "+e.getValue());
         }
+
+        System.out.println();
+
+        List<Library> lib = librariesService.findBook(book1);
+
+        for ( Library e  :  lib) {
+            System.out.println(e.toString());
+        }
+        LibraryService Serv1 = new LibraryService();
+
+        System.out.println();
+
+        System.out.println(one.getBooks());
+
+        Serv1.takeBook(book4 , new Person("Taras","Mykytiuk"), one);
+
+        System.out.println(one.getBooks());
+        System.out.println(one.getTakenBooks());
     }
 }
-
